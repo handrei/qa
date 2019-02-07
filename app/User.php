bbd7e76f10d5f95cefc0d93c866b5fe09364091c
handrei <h.andrei92@gmail.com>
2019-02-07 10:39:22 +0200

    add author infor abd question creation datein question item

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     public function questions(){
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute(){
+        // return route('questions.show', $this->id);
+        return "#";
+    }
 }
